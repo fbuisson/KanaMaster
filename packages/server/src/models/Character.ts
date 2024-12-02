1dd863487362fdd03c2fecd39849c5cd0bffc2d3
Florian Buisson <fb@bility.fr>
2024-12-02 11:27:00 +0100

    🦺 Add data validation before creating a character

--- packages/server/src/models/Character.ts
+++ packages/server/src/models/Character.ts
@@ -6,8 +6,10 @@ export type CharacterType = 'hiragana' | 'katakana' | 'kanji';
 export interface ICharacter extends Document {
   symbol: string;
   type: CharacterType;
-  vowel: 'a' | 'i' | 'u' | 'e' | 'o';
-  consonant: '-' | 'k' | 'g' | 's' | 'z' | 't' | 'd' | 'n' | 'h' | 'b' | 'p' | 'm' | 'y' | 'r' | 'w';
+  vowel?: 'a' | 'i' | 'u' | 'e' | 'o' | null;
+  consonant?: '-' | 'k' | 'g' | 's' | 'z' | 't' | 'd' | 'n' | 'h' | 'b' | 'p' | 'm' | 'y' | 'r' | 'w' | null;
+  japanese_pronunciation?: string | null;
+  translation?: string | null;
   media_url?: string;
 }
 
@@ -15,8 +17,16 @@ const CharacterSchema = new Schema<ICharacter>({
   symbol: { type: String, required: true, maxlength: 10 },
   type: { type: String, required: true, enum: ['hiragana', 'katakana'] },
   vowel: { type: String, required: true, enum: ['a', 'i', 'u', 'e', 'o'], maxlength: 1 },
-  consonant: { type: String, required: true, enum: ['-', 'k', 'g', 's', 'z', 't', 'd', 'n', 'h', 'b', 'p', 'm', 'y', 'r', 'w'], maxlength: 1 },
+  consonant: { type: String, enum: ['-', 'k', 'g', 's', 'z', 't', 'd', 'n', 'h', 'b', 'p', 'm', 'y', 'r', 'w'], maxlength: 1 },
+  translation: { type: String, maxlength: 255 },
   media_url: { type: String, maxlength: 255 },
+});
+
+CharacterSchema.pre('save', function(this: ICharacter, next: Function) {
+  if (this.vowel === null && this.consonant === null && this.translation === null && this.japanese_pronunciation === null) {
+    return next(new Error('Vowel, consonant, translation and japanese pronunciation cannot all be null at the same time.'));
+  }
+  next();
 });
 
 export default model<ICharacter>('Character', CharacterSchema);
